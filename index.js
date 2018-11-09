27bddd8f1d3e7543609c6777d6ce19ca35cd9d33
jocelyn <jocelyn@amtsolutions.fr>
2018-11-09 11:55:39 +0100

    add test script

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const { spawn } = require('child_process');
 const path = require('path');
 const argv = require('yargs').argv;
 
-if (argv._.length) process.env.WEBPACK_TURNKEY_ENTRY = argv._;
+if (argv._.length) {
+    process.env.WEBPACK_TURNKEY_ENTRY = argv._;
+}
 if (argv.eslint) {
     process.env.WEBPACK_TURNKEY_ESLINT = true;
+    delete argv['eslint'];
 }
 if (argv.sourmap) {
     process.env.WEBPACK_TURNKEY_SOURCEMAP = true;
+    delete argv['sourcemap'];
 }
+
+// Parse command options
 const options = require('./lib/options')(argv);
 if (process.env.NODE_ENV == 'prod') options.push(`-p`);
 else options.push(`-d`);
